c23bd4237a5b006ab34f56602088c5d90d77003c
CSOLSAN <csolsan@alumno.upv.es@ldsic-vdi11.upvnet.upv.es>
2023-11-28 19:39:42 +0100

    Añadido

---
+++ TableEntry.h
@@ -0,0 +1,23 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V> 
+class TableEntry {
+    public:
+	    std:: string key;
+	    V value;
+	    TableEntry(std::string key, V value){}
+	    TableEntry(std::string key){}
+	    TableEntry();
+	    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){}
+	    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){}
+	    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){}
+
+        // miembros públicos
+    
+};
+
+#endif
