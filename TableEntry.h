511784e088ce3cc6e6500f65745afa68f4976153
CSOLSAN <csolsan@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2024-01-08 19:13:28 +0100

    nuevo tableentry

--- TableEntry.h
+++ TableEntry.h
@@ -29,6 +29,18 @@ class TableEntry {
 	    	out << "Llave:" <<te.key << "/" << "Valor: " <<te.value;
 		return out;
 	    }
+	     friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		    if(te1.key < te2.key)
+			return true;
+	    	    else
+			return false;
+	    }
+	    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		    if(te1.key > te2.key)
+			    return true;
+		    else
+			    return false;
+	    }
 
         // miembros públicos
     
